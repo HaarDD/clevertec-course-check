ca060b137a6b78b5f8b8f9b46339646831c11311
HaarDD <maxgalitskiy@gmail.com>
2024-07-11 04:53:13 +0300

    feat: add runner class

---
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -0,0 +1,35 @@
+package main.java.ru.clevertec.check;
+
+import main.java.ru.clevertec.check.OrderService.Order;
+
+public class CheckRunner {
+
+    private static final String PATH_RESULT = "./src/main/resources/result.csv";
+
+    public static final Boolean CONSOLE_ADDITIONAL_INFO = true;
+
+    public static void main(String[] args) {
+
+        OrderService orderService = new OrderService();
+        CashierCheckService cashierCheckService = new CashierCheckService();
+
+        try {
+            Order order = orderService.parseFromConsoleArgs(args);
+            CashierCheck cashierCheck = cashierCheckService.getCashierCheckByOrder(order);
+
+            CSVWriter.writeToCSV(cashierCheck, PATH_RESULT);
+        } catch (Exception e) {
+            handleException(e);
+        }
+
+    }
+
+    private static void handleException(Exception e) {
+        try {
+            CSVWriter.writeToCSV(e, PATH_RESULT);
+        } catch (Exception ex) {
+            System.err.println("Failed to write exception to CSV: " + ex.getMessage());
+        }
+    }
+
+}
